c7a6e6bcab2356bbfb38906381661093a1018863
Matthew Jones <matthewjones@Matthews-MacBook-Air.local>
2017-10-18 14:22:04 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='userbased_nc_bench',
 	version='0.1',
-	description='Tests read parallel read rates from NetCDF4 files using Python and C',
+	description='Tests read parallel read rates from NetCDF4 files using Python and C. Limited functional at current.',
 	url='https://bitbucket.org/m_jones3/userbased-bench',
 	author='Matthew Jones',
 	author_email='m.jones3@pgr.reading.ac.uk',
